fb4fb43c02db4bfef598e2d6dc646bc59368f758
Orlandy Ariel <alu0100773408@ull.edu.es>
2016-10-20 16:28:22 +0100

    cambios menores

--- src/ull/patrones/calculadora/principal/Practica3PruebaEstrategia.java
+++ src/ull/patrones/calculadora/principal/Practica3PruebaEstrategia.java
@@ -8,15 +8,16 @@ import ull.patrones.calculadora.operaciones.Suma;
 
 public class Practica3PruebaEstrategia
 {
+	private static Contexto m_contexto;
 	public static void main(String[] args)
 	{
-		Contexto t_contexto= new Contexto(new Division());
-		System.out.println(t_contexto.ejecutarEstrategia(6, 2));
-		t_contexto = new Contexto(new Suma());
-		System.out.println(t_contexto.ejecutarEstrategia(3, 3));
-		t_contexto = new Contexto(new Resta());
-		System.out.println(t_contexto.ejecutarEstrategia(-4, 2));
-		t_contexto = new Contexto(new Multiplicacion());
-		System.out.println(t_contexto.ejecutarEstrategia(.3, 6));
+		m_contexto = new Contexto(new Division());
+		System.out.println(m_contexto.ejecutarEstrategia(6, 2));
+		m_contexto = new Contexto(new Suma());
+		System.out.println(m_contexto.ejecutarEstrategia(3, 3));
+		m_contexto = new Contexto(new Resta());
+		System.out.println(m_contexto.ejecutarEstrategia(-4, 2));
+		m_contexto = new Contexto(new Multiplicacion());
+		System.out.println(m_contexto.ejecutarEstrategia(.3, 6));
 	}
 }
